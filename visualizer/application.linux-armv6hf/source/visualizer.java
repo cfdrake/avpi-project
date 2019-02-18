1cd12f7137083d6d19dae75b41e978bb66bbec6a
Colin Drake <github@accounts.colindrake.me>
2019-02-18 11:00:16 -0600

    Work on visualizer, audio component, and server

--- visualizer/application.linux-armv6hf/source/visualizer.java
+++ visualizer/application.linux-armv6hf/source/visualizer.java
@@ -1,4 +1,22 @@
-import processing.net.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.net.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class visualizer extends PApplet {
+
+
 
 Server tcpServer;
 Client client;
@@ -7,13 +25,13 @@ int segmentSize = 10;
 int waveform = 1;
 float randomMagnitude = 30;
 
-void setup() {
-  size(900, 900);
+public void setup() {
+  
   tcpServer = new Server(this, 3443);
   client = new Client(this, "127.0.0.1", 3445);
 }
 
-void draw() {
+public void draw() {
   background(0);
 
   // Update data from Pd.
@@ -68,7 +86,7 @@ void draw() {
   for (int i = 0; i < width; i += segmentSize) {
     float midY = height / 2;
     float randOffset = random(randomMagnitude);
-    float drawY = midY + randOffset - (randomMagnitude * 0.5);
+    float drawY = midY + randOffset - (randomMagnitude * 0.5f);
 
     if (waveform == 1) {
       stroke(random(255));
@@ -82,5 +100,15 @@ void draw() {
     }
 
     line(i, drawY, i + segmentSize, drawY);
+  }
+}
+  public void settings() {  size(900, 900); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "--present", "--window-color=#000000", "--hide-stop", "visualizer" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
